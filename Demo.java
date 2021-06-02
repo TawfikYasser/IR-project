ac578e37c065f749c6333a50500c197d47642cd5
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-06-02 04:56:43 +0200

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,38 +1,81 @@
 import java.io.*;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Demo {
 
 	public static void main(String[] args) throws IOException {
-		
 		InvertedIndex index = new InvertedIndex();
-	        String phrase = "";
-
-	        index.buildIndex(new String[]{
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\100.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\101.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\102.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\103.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\104.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\105.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\106.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\107.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\108.txt", 
-	            "C:\\Users\\dell\\eclipse-workspace\\IR Project\\src\\tmp\\109.txt", 
-	        });
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-	        phrase = in.readLine();
-	        index.compare(phrase.toLowerCase());
-	         
-	        do {
-	        	
-	            System.out.println("Print search phrase: ");
-	            phrase = in.readLine();
-	            System.out.println(index.find(phrase.toLowerCase()));
-	           
-	        } while (!phrase.isEmpty());
-	        
+		// Building the index
+		index.buildIndex(new String[] { "C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\100.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\101.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\102.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\103.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\104.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\105.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\106.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\107.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\108.txt",
+				"C:\\Users\\tawfe\\eclipse-workspace\\IR Project\\src\\109.txt", });
+		System.out.println("Welcome to IR");
+		String phrase = "";
+		do {
+			System.out.println("1) Intersect.");
+			System.out.println("2) Find");
+			System.out.println("3) Find_01 [2 Terms]");
+			System.out.println("4) Find_02 [3 Terms]");
+			System.out.println("5) Find_03 [N Terms]");
+			System.out.println("6) Find_04 [N Terms] - Optimized");
+			System.out.println("7) Compare");
+			System.out.print("> ");
+		    Scanner choice = new Scanner(System.in);  // Create a Scanner object
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			int option = choice.nextInt();
+			switch (option) {
+			case 1:
+				HashSet<Integer> pL1 = new HashSet<Integer>();
+				HashSet<Integer> pL2 = new HashSet<Integer>();
+				HashSet<Integer> intersect_result = new HashSet<Integer>();
+				pL1.add(1);
+				pL1.add(2);
+				pL1.add(3);
+				pL2.add(1);
+				pL2.add(3);
+				intersect_result = index.intersect(pL1, pL2);
+				System.out.println(intersect_result);
+				phrase = "intersect";
+				break;
+			case 2:
+				System.out.println("Type a phrase: ");
+				phrase = in.readLine();
+				System.out.println(index.find(phrase.toLowerCase()));
+				break;
+			case 3:
+				System.out.println("Type a phrase with 2-terms: ");
+				phrase = in.readLine();
+				System.out.println(index.find_01(phrase.toLowerCase()));
+				break;
+			case 4:
+				System.out.println("Type a phrase with 3-terms: ");
+				phrase = in.readLine();
+				System.out.println(index.find_02(phrase.toLowerCase()));
+				break;
+			case 5:
+				System.out.println("Type a phrase with N-terms: ");
+				phrase = in.readLine();
+				System.out.println(index.find_03(phrase.toLowerCase()));
+				break;
+			case 6:
+				System.out.println("Type a phrase with N-terms for optimized: ");
+				phrase = in.readLine();
+				System.out.println(index.find_04(phrase.toLowerCase()));
+				break;
+			case 7:
+				System.out.println("Type a phrase for compare: ");
+				phrase = in.readLine();
+				index.compare(phrase.toLowerCase());
+				break;
+			}
+		} while (!phrase.isEmpty());
 	}
-
 }
-
-
